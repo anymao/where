666fe7f88c66f5b55339b1917dc717771c73f186
liuyuanmao <liuyuanmao@xiaomai5.com>
2021-06-09 16:54:25 +0800

    publish runtime lib

--- where-plugin/build.gradle.kts
+++ where-plugin/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 repositories {
     google()
-    jcenter()
 }
 
 dependencies {
-    implementation(gradleApi())
-    implementation(localGroovy())
+    compileOnly(gradleApi())
+    compileOnly(localGroovy())
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.31")
     implementation("com.android.tools.build:gradle:4.1.3")
     implementation("commons-io:commons-io:2.8.0")
     implementation("commons-codec:commons-codec:1.15")
